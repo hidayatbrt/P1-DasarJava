a174b4635a0d27ecedbfe842856c358a920ec9c0
hidayat <dayatnew2@gmail.com>
2022-03-14 15:10:39 +0700

    initial projek pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,12 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.print("Pemrograman Berorientasi Objek dengan Java");
+        //Contoh Komentar satu baris
+        /*
+        contoh komentar lebih dari satu baris
+         */
+        /**
+         * java Doc --> baris ini untuk mendokumentasi kode program
+         */
+    }
+}
