204079ab3829fa2adb3bed663e890e2b84e9295b
hidayat <dayatnew2@gmail.com>
2022-03-14 15:32:40 +0700

    percobaan whitepace dijava

---
+++ src/B_BlokDanKomentar.java
@@ -0,0 +1,4 @@
+/**
+ * contoh penggunaan whitepace dijava
+ */
+public class B_BlokDanKomentar { public static void main(String[] args) { System.out.println("Pemrograman Berorientasi Objek dengan Java"); }}
