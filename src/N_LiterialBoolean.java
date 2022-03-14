2e0c0ce2b05bb79317291b9bd4b09a66992feede
hidayat <dayatnew2@gmail.com>
2022-03-14 19:43:37 +0700

    percobaan mencetak literial boolean dijava

---
+++ src/N_LiterialBoolean.java
@@ -0,0 +1,8 @@
+public class N_LiterialBoolean {
+    public static void main(String[] args) {
+        boolean isOpen = true;
+        boolean isClose = false;
+        System.out.println("contoh literal boolean kondisi" +isClose);
+        System.out.println(" contoh litereal boolean kondisi" + isClose);
+    }
+}
