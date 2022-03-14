2fc3684cdc5bd6f76447c8a6383f38be2796be96
hidayat <dayatnew2@gmail.com>
2022-03-14 16:31:56 +0700

    percobaan variabel dijava

---
+++ src/C_Variabel.java
@@ -0,0 +1,10 @@
+public class C_Variabel {
+    public static void main(String[] args) {
+        double salary;
+        long _distance;
+        boolean $flag;
+    }
+}
+/**
+ * contoh penggunaan variabel dijava
+ */
