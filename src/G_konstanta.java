1bf696f5aec3626fd8e6898d9dcb1493f246e86b
hidayat <dayatnew2@gmail.com>
2022-03-14 17:14:59 +0700

    percobaan mencetak nilai konstanta dijava

---
+++ src/G_konstanta.java
@@ -0,0 +1,11 @@
+public class G_konstanta {
+    public static void main(String[] args) {
+        final double PHI =3.14;
+        final double CM_PER_INCH = 2.54;
+        System.out.println("Nilai Konstanta PHI = "+ PHI);
+        System.out.println("Nilai Konstanta CM_PER_INCH =" + CM_PER_INCH);
+    }
+}
+/**
+ * contoh penggunaan nilai konstanta dijava
+ */
