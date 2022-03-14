eb9275b4ebb5d3de594f342f2716d655ac0fffca
hidayat <dayatnew2@gmail.com>
2022-03-14 19:41:32 +0700

    percobaan mencetak literial string dijava

---
+++ src/M_LiterialString.java
@@ -0,0 +1,6 @@
+public class M_LiterialString {
+    public static void main(String[] args) {
+        String welcome ="pemrongraman berorientasi objek dengan java";
+        System.out.println("welcome");
+    }
+}
